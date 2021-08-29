00410e2e9d46cb3c5fdc534a1a6af4fce5edff77
r.sakthivel <r.sakthivel@accenture.com>
2021-08-29 14:46:54 +0530

    missed file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/Layout/header/header.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
